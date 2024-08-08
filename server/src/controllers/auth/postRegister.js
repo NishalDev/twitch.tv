60d9b8a8da212a4e56d6650befc744f226c2996c
Nishal <nishaldevadiga2003@gmail.com>
2024-08-08 18:48:36 +0530

    issuingJwtToken

--- server/src/controllers/auth/postRegister.js
+++ server/src/controllers/auth/postRegister.js
@@ -1,11 +1,49 @@
 import User from "../../models/User.js";
-
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 export const postRegister = async (req, res) => {
-    const user = await User.create({
-        username: 'Mark',
-        email: 'testemail@ad.com',
-        password: "password",
-    })
+    try{
+        const { username, email, password} = req.body;
+        const userExists = await User.exists({ email });
+        if(userExists){
+            return res.status(409).send("E-mail already in use");
+        }
+        const encrytedPassword = await bcrypt.hash(password, 10);
+
+        const user = await User.create({
+            username,
+            email: email.toLowerCase(),
+            password: encrytedPassword,
+        })
+
+        //create JWT token
+        const token = jwt.sign(
+            // user details which we would like to encrypt in jWT token
+            {
+                userId: user._id,
+                email,
+            },
+            // secret
+            process.env.TOKEN_KEY,
+            // additional config
+            {
+                expiresIn: "8h",
+            }
+        );
+        
+        //send success response back to the user with data of the registered user and JWT  
+        return res.status(201).json({
+            userDetails: {
+                email,
+                username,
+                token,
+            },
+        })
+        
+    }catch(err){
+        console.log(err);
+        return res.status(500).send('Error occured. Please try again')
+    }
     return res.send("User has been added to database");
 };
  
