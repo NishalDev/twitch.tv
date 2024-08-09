eb7105aefbf24447a7de09da0cd9ad3566e157b6
Nishal <nishaldevadiga2003@gmail.com>
2024-08-09 19:35:03 +0530

    loginRegisterComponentCreation

--- client/src/AuthPage/AuthPage.js
+++ client/src/AuthPage/AuthPage.js
@@ -1,9 +1,17 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Login } from "./Login";
+import { Register } from "./Register";
 export const AuthPage = () => {
-    return (
-        <div>
-            Hello here is Auth page
-        </div>
-    );
+    const [islogin, setIsLogin] = useState(true);
+    const handleAuthPageToggle = () => {
+        setIsLogin((prev) => !prev);
+    };
+
+    return <div>
+        {islogin ? (
+    <Login switchAuthHandler = {handleAuthPageToggle}/> 
+        ) : (
+    <Register switchAuthHandler = {handleAuthPageToggle}/>
+    )}
+    </div>;
 };
---
+++ client/src/AuthPage/Login.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Login = ({switchAuthHandler}) => {
+    return (
+        <div onClick={switchAuthHandler}>
+            Login
+        </div>
+    );
+};
