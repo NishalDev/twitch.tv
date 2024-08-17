bfa0aff280a60c2f6f261c63ce7f89ca7c24c824
Nishal <nishaldevadiga2003@gmail.com>
2024-08-17 11:41:57 +0530

    LoginComponentContainerFormatting_1

--- client/src/AuthPage/AuthPage.js
+++ client/src/AuthPage/AuthPage.js
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 import { Login } from "./Login";
 import { Register } from "./Register";
+import "./authPage.css";
 export const AuthPage = () => {
     const [islogin, setIsLogin] = useState(true);
     const handleAuthPageToggle = () => {
         setIsLogin((prev) => !prev);
-    };
+    };          
 
-    return <div>
+    return <div className="auth-container">
         {islogin ? (
     <Login switchAuthHandler = {handleAuthPageToggle}/> 
         ) : (
--- client/src/AuthPage/Login.js
+++ client/src/AuthPage/Login.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { Logo } from "./Logo";
 
 export const Login = ({switchAuthHandler}) => {
     return (
-        <div onClick={switchAuthHandler}>
-            Login
+        <div class="login-container">
+           <Logo text={'Log in to Clone'}  />
+            <form className="auth-form">
+                Form
+            </form>
+            <span onClick={switchAuthHandler} className="auth-form-switch-label">
+                Don't have an Account? sign up
+            </span>
         </div>
     );
 };
